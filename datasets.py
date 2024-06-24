7ef93da43c32a1a4d93764c144e10361e1b491d4
Aditya <adityanandy2802@gmail.com>
2024-06-24 20:52:55 +0530

    update requirements

--- datasets.py
+++ datasets.py
@@ -1,3 +1,3 @@
 passengers_url = 'https://raw.githubusercontent.com/tohid-yousefi/Airline_Passengers_Forecasting_Using_Time_Series_Methods/main/AirPassengers.csv'
-# shampoo_url = "sales_of_shampoo_over_a_three_ye.csv"
+shampoo_url = "sales_of_shampoo_over_a_three_ye.csv"
 stockmarket_url = "https://raw.githubusercontent.com/abulbasar/data/master/tcs-stock.csv"
